eceaf3d2fb04f3ad061da019e6c70d8f44aeae49
ArnavYaduvanshi <67420391+ArnavYaduvanshi@users.noreply.github.com>
2025-06-29 08:44:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import{redirect} from 'next/navigation';
 export default function Page() {
   redirect('/optimus_media/home');
-  return null; // This line is never reached, but it's good practice to return something.
+  return null; 
 }
